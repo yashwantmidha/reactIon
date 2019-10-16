8d1e3c189f121e0008c5c0f3f840822eeac5afa9
macbook <macbook@macbooks-MacBook-Pro.local>
2019-10-16 13:21:24 -0400

    new

---
+++ screens/HomeScreen.js
@@ -0,0 +1,65 @@
+import { ScrollView, StyleSheet } from 'react-native';
+import React, { Component } from "react";
+import { Text, View, TouchableOpacity } from "react-native";
+import { string } from "prop-types";
+
+class HomeScreen extends Component {
+  state = {
+    data: []
+  };
+  componentDidMount = () => {
+    this.fetchData();
+  };
+  fetchData = async () => {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    );
+    const json = await response.json();
+    this.setState({ data: json });
+  };
+
+  alertItemName = item => {
+    const main =
+      "Email:" + item.email + "\nName:" + item.name + "\nBody:" + item.body;
+    alert(main);
+  };
+  render() {
+    return (
+      
+      <ScrollView style={styles.container}>
+<View>
+        {this.state.data.map((item, index) => (
+          <TouchableOpacity
+            key={item.id}
+            style={styles.container}
+           onPress={() => this.props.navigation.navigate('LinksStack',
+           {
+             items : item
+           })}
+           
+           
+          >
+            <Text style={styles.text}>{item.email}</Text>
+          </TouchableOpacity>
+        ))}</View>
+      </ScrollView>
+      
+    );
+  }
+}
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+    padding: 10,
+    marginTop: 3,
+    //backgroundColor: "#d9f9b1",
+    //alignItems: "center"
+  },
+  text: {
+    color: "#4f603c"
+  }
+});
---
+++ screens/LinksScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
+import { ExpoLinksView } from '@expo/samples';
+import HomeScreen from './HomeScreen';
+
+export default function LinksScreen() {
+  return (
+    <ScrollView style={styles.container}>
+      <text>{HomeScreen.Email}</text>
+    </ScrollView>
+  );
+}
+
+LinksScreen.navigationOptions = {
+  title: 'Links',
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+});
