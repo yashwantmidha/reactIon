45225bed8b59ad42838bbf4947dfc7e582575661
macbook <macbook@macbooks-MacBook-Pro.local>
2019-10-17 13:19:58 -0400

    first commit

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { ScrollView, StyleSheet,Text } from 'react-native';
+//import { ExpoLinksView } from '@expo/samples';
 
 export default function LinksScreen() {
   return (
     <ScrollView style={styles.container}>
-      {/**
-       * Go ahead and delete ExpoLinksView and replace it with your content;
-       * we just wanted to provide you with some helpful links.
-       */}
-      <ExpoLinksView />
+      <Text>Hello Hello! 
+      This Page will Be Out Soon</Text>
     </ScrollView>
   );
 }
